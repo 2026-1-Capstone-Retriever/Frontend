830bd7fea177aa803687bd165fda620026fe7fed
jooya38 <hyj0102809@gmail.com>
2026-02-26 21:26:07 +0900

    [FEAT] home UI 구현

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
+
 import 'package:safepath/common/widgets/logo_widget.dart';
+import 'package:safepath/common/widgets/button_widget.dart';
+import 'package:safepath/common/theme/text_styles.dart';
+import 'package:safepath/common/theme/color_collection.dart';
+import 'package:safepath/routes/app_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(child: LogoWidget())),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 33),
+            const Center(child: LogoWidget()),
+            const SizedBox(height: 49),
+            Expanded(
+              child: CustomButton(
+                width: 346,
+                icon: Icons.remove_red_eye,
+                iconColor: ColorCollection.main,
+                iconSize: 38,
+                title: '장애물 탐지',
+                subtitle: '실시간으로 보행 위험물을 감지합니다.',
+                backgroundColor: ColorCollection.background,
+                titleColor: ColorCollection.main,
+                titleStyle: AppTextStyles.title2,
+                subtitleColor: ColorCollection.point,
+                subtitleStyle: AppTextStyles.labelRegular,
+                borderColor: ColorCollection.main,
+              ),
+            ),
+            const SizedBox(height: 32),
+            Expanded(
+              child: CustomButton(
+                width: 346,
+                icon: Icons.location_on,
+                iconColor: ColorCollection.main,
+                iconSize: 38,
+                title: '스마트 길찾기',
+                subtitle: '가장 안전한 경로로 안내합니다.',
+                backgroundColor: ColorCollection.background,
+                titleColor: ColorCollection.main,
+                titleStyle: AppTextStyles.title2,
+                subtitleColor: ColorCollection.point,
+                subtitleStyle: AppTextStyles.labelRegular,
+                borderColor: ColorCollection.main,
+              ),
+            ),
+            const SizedBox(height: 32),
+            Expanded(
+              child: CustomButton(
+                width: 346,
+                icon: Icons.settings,
+                iconColor: ColorCollection.main,
+                iconSize: 38,
+                title: '설정',
+                subtitle: '사용자 맞춤 설정을 진행합니다.',
+                backgroundColor: ColorCollection.background,
+                titleColor: ColorCollection.main,
+                titleStyle: AppTextStyles.title2,
+                subtitleColor: ColorCollection.point,
+                subtitleStyle: AppTextStyles.labelRegular,
+                borderColor: ColorCollection.main,
+              ),
+            ),
+            const SizedBox(height: 32),
+          ],
+        ),
+      ),
     );
   }
 }
