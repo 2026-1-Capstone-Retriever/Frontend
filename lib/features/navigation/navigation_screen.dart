05a99ab1bcead0635ec038cfbfa1091730ff57a0
jooya38 <hyj0102809@gmail.com>
2026-03-05 10:50:54 +0900

    [FEAT] CustomTitleBar 구현 및 적용

--- lib/features/navigation/navigation_screen.dart
+++ lib/features/navigation/navigation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:safepath/common/theme/text_styles.dart';
 import 'package:safepath/common/theme/color_collection.dart';
+import 'package:safepath/common/widgets/title_bar_widget.dart';
 
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
@@ -8,14 +9,7 @@ class NavigationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '길찾기',
-          style: AppTextStyles.title2.copyWith(color: ColorCollection.point),
-        ),
-        backgroundColor: ColorCollection.main,
-        iconTheme: const IconThemeData(color: ColorCollection.point),
-      ),
+      appBar: CustomTitleBar(title: '길찾기'),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
