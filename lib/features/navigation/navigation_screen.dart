5c22ae718a34a93dd61bc37d18010673f8c7a9ff
hanyj0317 <hyj0931@naver.com>
2026-03-06 23:55:17 +0900

    [FEAT] 길찾기 - 저장된 장소, 최근 장소 UI 구현

--- lib/features/navigation/navigation_screen.dart
+++ lib/features/navigation/navigation_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:safepath/common/enum/place_category.dart';
 import 'package:safepath/common/theme/text_styles.dart';
 import 'package:safepath/common/theme/color_collection.dart';
 import 'package:safepath/common/widgets/title_bar_widget.dart';
+import 'package:safepath/features/navigation/edit_button.dart';
+import 'package:safepath/features/navigation/saved_place_widget.dart';
 
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
@@ -16,12 +19,36 @@ class NavigationScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '저장된 장소',
+                    style: AppTextStyles.title2.copyWith(
+                      color: ColorCollection.point,
+                      fontSize: 20,
+                    ),
+                  ),
+                  EditButton(),
+                ],
+              ),
+              const SizedBox(height: 25),
+              SavedPlaceWidget(
+                label: '집',
+                location: '서울특별시 성북구 솔샘로8길',
+                category: PlaceCategory.home,
+              ),
+              const SizedBox(height: 25),
               Text(
-                '길찾기',
+                '최근 장소',
                 style: AppTextStyles.title2.copyWith(
                   color: ColorCollection.point,
+                  fontSize: 20,
                 ),
               ),
+              const SizedBox(height: 25),
+              SavedPlaceWidget(label: '집', location: '서울특별시 성북구 솔샘로8길'),
             ],
           ),
         ),
