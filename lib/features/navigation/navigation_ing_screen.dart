5ec5e1d37b14872493090e1795b2b5586ee027bc
hanyj0317 <hyj0931@naver.com>
2026-03-30 19:08:26 +0900

    [FEAT] 길찾기 안내 중 페이지 구현

--- lib/features/navigation/navigation_ing_screen.dart
+++ lib/features/navigation/navigation_ing_screen.dart
@@ -3,7 +3,10 @@ import 'package:safepath/common/theme/color_collection.dart';
 import 'package:safepath/common/theme/text_styles.dart';
 import 'package:safepath/common/widgets/action_button_widget.dart';
 import 'package:safepath/common/widgets/title_bar_widget.dart';
+import 'package:safepath/features/navigation/navigation_step_card.dart';
+import 'package:safepath/features/navigation/navigation_voiceguide_card.dart';
 import 'package:safepath/routes/app_router.dart';
+import 'package:safepath/features/navigation/navigation_overview_card.dart';
 
 class NavigationIngScreen extends StatefulWidget {
   const NavigationIngScreen({super.key});
@@ -20,27 +23,47 @@ class _NavigationIngScreenState extends State<NavigationIngScreen> {
       child: Scaffold(
         appBar: CustomTitleBar(title: '길찾기'),
         body: SafeArea(
+          bottom: false,
           child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '길찾기 중',
-                    style: AppTextStyles.labelRegular.copyWith(
-                      color: ColorCollection.point,
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        NavigationOverviewCard(
+                          distance: 850,
+                          time: 12,
+                          status: RouteStatus.safe,
+                        ),
+                        const SizedBox(height: 20),
+                        NavigationStepCard(
+                          direction: DirectionType.straight,
+                          instruction: '100m 앞에서 우회전하세요.',
+                          distance: 100,
+                        ),
+                        const SizedBox(height: 20),
+                        NavigationVoiceGuideCard(
+                          voiceGuide: '50미터 앞 오른쪽에 킥보드가 감지되었습니다.',
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 30),
-                  ActionButton(
-                    label: '안내 중지',
-                    icon: Icons.stop_circle_outlined,
-                    backgroundColor: ColorCollection.red,
-                    onTap: () => Navigator.pop(context),
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 24),
+                    child: ActionButton(
+                      label: '안내 중지',
+                      icon: Icons.stop_circle_outlined,
+                      backgroundColor: ColorCollection.red,
+                      onTap: () => Navigator.pop(context),
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
